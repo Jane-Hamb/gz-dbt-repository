a8a1aec11801b5e1584588a3432d9031a67e99a8
Yevheniia Heier <chiaradnepr@gmail.com>
2024-12-17 16:01:39 +0000

    add ship costs in finance day

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,15 +1,13 @@
 WITH campaigns_day_data AS (
-    SELECT
-        date_date,  
-        SUM(ads_cost) AS ads_cost,  
-        SUM(impression) AS ads_impressions,  
-        SUM(click) AS ads_clicks,  
-         
-    FROM
-       {{ ref('int_campaigns') }}
-    
-    GROUP BY
-        date_date  
+    SELECT 
+        date_date,
+        SUM(ads_cost) AS ads_cost,
+        SUM(impression) AS ads_impressions,
+        SUM(click) AS ads_clicks
+    FROM 
+        {{ ref('int_campaigns') }}  
+    GROUP BY 
+        date_date
 )
 
 SELECT * FROM campaigns_day_data
